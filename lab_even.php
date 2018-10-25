e928137ca18b80dd59758ef85cdd0f6a1f370d3b
p-rit <36683884+p-rit@users.noreply.github.com>
2018-10-25 03:58:13 -0700

    Update lab_even.php

--- lab_even.php
+++ lab_even.php
@@ -8,7 +8,7 @@
       <meta name="description" content="">
       <meta name="author" content="">
 
-      <title>Timetable Generation System</title>
+      <title>Timetable Management System</title>
 
       <!-- Bootstrap core CSS -->
       <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
