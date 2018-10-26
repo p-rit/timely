01bf120b7c600d12650e5172fc607c87af388d6f
ritul10 <artistritul1995@gmail.com>
2018-10-25 17:35:34 -0700

    deleted a folder

--- admin/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
- session_start();
-session_destroy(); 
-header('LOCATION:../timetable.php');
-?>
--- admin/time/dbConfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn=mysqli_connect("localhost","root","","timetable");
-if(!$conn)
-{
-die("Connection failed: " . mysqli_connect_error());
-}
-?>
